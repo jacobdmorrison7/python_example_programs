7590536ee27a3a4415a5684c6cad71fd680aee73
Jacob <jacobdmorrison7@gmail.com>
2021-09-24 12:18:02 -0500

    try,except

--- first_largest.py
+++ first_largest.py
@@ -2,15 +2,22 @@
 Python program to find the largest element and its location.
 """
 
-def largest_element(a, loc):
+def largest_element(a, loc = False):
     """ Return the largest element of a sequence a.
     """
     maxval = a[0]
     val_loc = 0
-    for i in range(1, len(a)):
-        if a[i]> maxval:
-            maxval = a[i]
-            val_loc = i
+    try:
+        for i in range(1, len(a)):
+            if a[i]> maxval:
+                maxval = a[i]
+                val_loc = i
+    except TypeError:
+        print("Values must all be of the same type")
+        return
+    except:
+        print("Unidentified error")
+        return
 
     if loc==True:
         return maxval, val_loc
@@ -18,7 +25,8 @@ def largest_element(a, loc):
 
 
 
+
 if __name__ == "__main__":
 
-    a = [1,2,3,2,1]
-    print("Largest element is {:}".format(largest_element(a, loc = False)))
+    a = [1,2,'a',2,1]
+    print("Largest element is {:}".format(largest_element(a)))
